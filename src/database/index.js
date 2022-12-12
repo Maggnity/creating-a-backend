5e6907ffbad0486373e9add5a0f0b82d932b4730
Fernando-Hooklab <109971336+Fernando-Hooklab@users.noreply.github.com>
2022-12-11 21:40:36 -0300

    Salvando fotos na base de dados

--- src/database/index.js
+++ src/database/index.js
@@ -1,13 +1,16 @@
 import { Sequelize } from "sequelize";
 import database from "../config/database";
 import Aluno from "../models/alunoModel";
+import Foto from "../models/fotoModel";
 import User from "../models/userModel";
 
 const models = [
 	Aluno,
-	User
+	User,
+	Foto
 ];
 
 const connection = new Sequelize(database)
 
 models.forEach(model => model.init(connection))
+models.forEach(model => model.associate && model.associate(connection.models))
---
+++ src/models/fotoModel.js
@@ -0,0 +1,34 @@
+import Sequelize, { Model } from 'sequelize';
+
+export default class Foto extends Model {
+	static init(sequelize) {
+		super.init({
+			originalname: {
+				type: Sequelize.STRING,
+				defaultValue: '',
+				validate: {
+					notEmpty: {
+						msg: 'Campo não pode ser vazioooo'
+					}
+				}
+			},
+			filename: {
+				type: Sequelize.STRING,
+				defaultValue: '',
+				validate: {
+					notEmpty: {
+						msg: 'Campo não pode ser vazio'
+					}
+				}
+			},
+
+		}, {
+			sequelize,
+		})
+		return this;
+	}
+
+	static associate(models) {
+		this.belongsTo(models.Aluno, { foreignKey: 'aluno_id'})
+	}
+}
